f77b8bc2a92f9ef851a0d3ec429181b8a564fe0e
Eduardo_vl <eduardovl_27@outlook.com>
2023-11-19 15:34:13 -0600

    prueba

--- Token.java
+++ Token.java
@@ -3,17 +3,20 @@ public class Token {
     final TipoToken tipo;
     final String lexema;
     final int posicion;
+    final Object literal;
 
     public Token(TipoToken tipo, String lexema, int posicion) {
         this.tipo = tipo;
         this.lexema = lexema;
         this.posicion = posicion;
+        this.literal = null;
     }
 
-    public Token(TipoToken tipo, String lexema) {
+    public Token(TipoToken tipo, String lexema, Object literal) {
         this.tipo = tipo;
         this.lexema = lexema;
         this.posicion = 0;
+        this.literal = literal;
     }
 
     @Override
@@ -31,5 +34,23 @@ public class Token {
 
     public String toString(){
         return tipo + " " + lexema + " ";
+    }
+
+
+
+ public TipoToken getTipo() {
+        return tipo;
+    }
+
+    public String getLexema() {
+        return lexema;
+    }
+
+    public Object getLiteral() {
+        return literal;
+    }
+
+    public int getPosicion() {
+        return posicion;
     }
 }
